4f996cb3b9a02dfb17c6afb1e2bee80cc8273ba9
Koushik Dey <koushik.dey@live.com>
2022-05-19 23:16:39 +0300

    initial-6

--- e-retail/src/App.jsx
+++ e-retail/src/App.jsx
@@ -1,13 +1,18 @@
 import logo from './logo.png';
 import './App.css';
+import Home from './pages/Home';
 
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          e-retail store@underdevelopment...
+        </p>
       </header>
+      <Home />
     </div>
   );
 };
