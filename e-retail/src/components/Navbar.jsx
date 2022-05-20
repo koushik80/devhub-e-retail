ad7e7cc427abc1d06c5e82e3070cf9d58931c137
Koushik Dey <koushik.dey@live.com>
2022-05-21 01:49:04 +0300

    navbar-4

--- e-retail/src/components/Navbar.jsx
+++ e-retail/src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import logo from './logo.png';
 import '../App.css';
-import { Search } from "@material-ui/icons";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 
 const Container = styled.div`
      height: 60px;
@@ -45,11 +46,23 @@ const Center = styled.div`
   text-align: center;
 `;
 
+const Logo = styled.h1`
+  font-weight: bold;
+ 
+`;
+
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  
 `;
 
 const Navbar = () => {
@@ -63,9 +76,16 @@ const Navbar = () => {
                <Search style={{ color: "gray", fontSize: 16 }} />
              </SearchContainer>
             </Left>
-            <Center>Center
+            <Center><Logo>devHuß</Logo>
             </Center>
-            <Right>Right
+            <Right>
+              <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
             </Right>
           </Wrapper>
            <img src={logo} className="App-logo" alt="logo" />
