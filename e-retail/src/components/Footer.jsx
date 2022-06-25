783cff277b63d5c066f937bca1d8f9f3046a3f4a
koushik <koushik.dey@live.com>
2022-06-26 01:14:07 +0300

    ftr2

--- e-retail/src/components/Footer.jsx
+++ e-retail/src/components/Footer.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import styled from 'styled-components';
+import { mobile } from '../responsive';
+
+const Container = styled.div`
+
+`;
 
 const Footer = () => {
     return (
-        <div>
-      
-        </div>
+        <Container>
+
+        </Container>
     );
 };
 
