f98030f8568be2f727f24a6607e151ed5e144c2c
koushik <koushik.dey@live.com>
2022-06-25 23:43:15 +0300

    stl

--- e-retail/src/components/Newsletter.jsx
+++ e-retail/src/components/Newsletter.jsx
@@ -1,11 +1,36 @@
 import styled from 'styled-components';
 import { Send } from '@material-ui/icons';
 
+
+const Container = styled.div`
+
+`;
+
+const Title = styled.div`
+
+`;
+
+const Desc = styled.div`
+
+`;
+
+const InputContainer = styled.div`
+
+`;
+
+const Input = styled.div`
+
+`;
+
+const Button = styled.div`
+
+`;
+
 const Newsletter = () => {
     return (
         <Container>
             <Title></Title>
-            <Description></Description>
+            <Desc></Desc>
             <InputContainer>
                 <Input />
                 <Button>
