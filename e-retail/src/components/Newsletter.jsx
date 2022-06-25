a1550422fbf9fdff276d16af86d90fe7c82e29c3
koushik <koushik.dey@live.com>
2022-06-26 00:16:30 +0300

    nwsl3

--- e-retail/src/components/Newsletter.jsx
+++ e-retail/src/components/Newsletter.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Send } from '@material-ui/icons';
+import { mobile } from '../responsive';
 
 
 const Container = styled.div`
@@ -12,11 +13,15 @@ const Container = styled.div`
 `;
 
 const Title = styled.h1`
-
+  font-size: 70px;
+  margin-bottom: 20px;
 `;
 
 const Desc = styled.div`
-
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })}
 `;
 
 const InputContainer = styled.div`
