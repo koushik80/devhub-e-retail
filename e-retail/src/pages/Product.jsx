d91ce11df4d41dc621fc399cf2bf2a05ec091e4d
koushik <koushik.dey@live.com>
2022-06-29 02:56:45 +0300

    reg

--- e-retail/src/pages/Product.jsx
+++ e-retail/src/pages/Product.jsx
@@ -104,7 +104,14 @@ const Amount = styled.span`
 `;
 
 const Button = styled.button`
-
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
 `;
 
 
