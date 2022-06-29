384c5f34a469b384836968edd9c8e31718793a2b
koushik <koushik.dey@live.com>
2022-06-29 11:28:49 +0300

    bckgrnd

--- e-retail/src/pages/Register.jsx
+++ e-retail/src/pages/Register.jsx
@@ -11,6 +11,10 @@ const Container = styled.div`
     ),
     url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
       center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Wrapper = styled.div`
