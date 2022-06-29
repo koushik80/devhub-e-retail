c3762d3f65cfcb541ed364e11b7d917776bcf37b
koushik <koushik.dey@live.com>
2022-06-29 10:58:15 +0300

    frm

--- e-retail/src/pages/Register.jsx
+++ e-retail/src/pages/Register.jsx
@@ -15,11 +15,29 @@ const Title = styled.h1`
 
 `;
 
+const Form = styled.form`
+
+`;
+
+const Input = styled.form`
+
+`;
+
+
+
 const Register = () => {
     return (
         <Container>
             <Wrapper>
                 <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+                    <Input placeholder="first name" />
+                    <Input placeholder="last name" />
+                    <Input placeholder="username" />
+                    <Input placeholder="email" />
+                    <Input placeholder="password" />
+                    <Input placeholder="confirm password" />
+                </Form>
            </Wrapper>
         </Container>
     );
