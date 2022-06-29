ca7db081da53ae2c745d8c2191b4075eae5d98dd
koushik <koushik.dey@live.com>
2022-06-29 12:45:41 +0300

    crt3

--- e-retail/src/pages/Cart.jsx
+++ e-retail/src/pages/Cart.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import styled from 'styled-components';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+const Container = styled.div`
+
+`;
+
+
+
+
 
 const Cart = () => {
     return (
-        <div>
-      
-        </div>
+        <Container>
+            <Navbar />
+            <Announcement />
+        </Container>
     );
 };
 
