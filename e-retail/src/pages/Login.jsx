cf675331fd344a280936a720b79b718ab5acc38e
koushik <koushik.dey@live.com>
2022-06-29 12:29:37 +0300

    lgn

--- e-retail/src/pages/Login.jsx
+++ e-retail/src/pages/Login.jsx
@@ -1,14 +1,35 @@
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
 `;
 
+const Wrapper = styled.div`
+
+`;
+
+
+
+
+
+
+
+
 const Login = () => {
     return (
         <Container>
-      
+         <Wrapper>
+          <Title>SIGN IN</Title>
+          <Form>
+            <Input placeholder="Username" />
+            <Input placeholder="Password" />
+            <Button>LOGIN</Button>
+            <Link>DON'T YOU REMEMBER THE PASSWORD?</Link>
+            <Link>CREATE A NEW ACCOUNT</Link>
+          </Form>
+         </Wrapper>
         </Container>
     );
 };
