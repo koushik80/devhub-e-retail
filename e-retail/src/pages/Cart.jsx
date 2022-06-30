92fe38c4a145fc8d505dfcf551931bb40bf8abdd
koushik <koushik.dey@live.com>
2022-06-30 16:55:52 +0300

    prdt-dst

--- e-retail/src/pages/Cart.jsx
+++ e-retail/src/pages/Cart.jsx
@@ -70,6 +70,26 @@ const Image = styled.img`
   width: 200px;
 `;
 
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
 const PriceDetail = styled.div`
   flex: 1;
   display: flex;
@@ -111,6 +131,18 @@ const Cart = () => {
                         <Product>
                             <ProductDetail>
                                 <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
+                                <Details>
+                                    <ProductName>
+                                          <b>Product:</b> JESSIE THUNDER SHOES
+                                    </ProductName>
+                                    <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
+                                </Details>
                             </ProductDetail>
                             <PriceDetail>price</PriceDetail>
                         </Product>
