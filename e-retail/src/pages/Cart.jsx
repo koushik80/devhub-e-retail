8ccdef4917f3de4b553cba5aad5f2a7dcb3fb9e8
koushik <koushik.dey@live.com>
2022-06-30 17:10:12 +0300

    prc dtl

--- e-retail/src/pages/Cart.jsx
+++ e-retail/src/pages/Cart.jsx
@@ -133,18 +133,25 @@ const Cart = () => {
                                 <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
                                 <Details>
                                     <ProductName>
-                                          <b>Product:</b> JESSIE THUNDER SHOES
+                                      <b>Product:</b> JESSIE THUNDER SHOES
                                     </ProductName>
                                     <ProductId>
-                    <b>ID:</b> 93813718293
-                  </ProductId>
-                  <ProductColor color="black" />
-                  <ProductSize>
-                    <b>Size:</b> 37.5
-                  </ProductSize>
+                                      <b>ID:</b> 93813718293
+                                    </ProductId>
+                                    <ProductColor color="black" />
+                                    <ProductSize>
+                                      <b>Size:</b> 37.5
+                                    </ProductSize>
                                 </Details>
                             </ProductDetail>
-                            <PriceDetail>price</PriceDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                  <Add />
+                                  <ProductAmount>2</ProductAmount>
+                                  <Remove />
+                                  </ProductAmountContainer>
+                                  <ProductPrice>€ 30</ProductPrice>
+                            </PriceDetail>
                         </Product>
                     </Info>
                     <Summary>summary</Summary>
